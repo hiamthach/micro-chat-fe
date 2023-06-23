f5cdd29ef582027fc316ebc92b55eef004ba25dc
Ngoc Thach <thach.le@topebox.com>
2023-06-23 18:14:13 +0700

    test pb

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import HomePage from "./components/HomePage"
+import HomePage from './components/HomePage';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { ChatServiceClient } from './ChatServiceClientPb';
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const client = new ChatServiceClient('http://localhost:8080', null, null);
 
 function App() {
-
   return (
-    <HomePage/>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,16 +1,29 @@
-import { Button, Input, Divider } from "antd"
+import { Button, Input, Divider, Form } from 'antd';
 
 const HomePage = () => {
+  const handleCreateRoom = () => {
+    console.log('create room');
+  };
+
   return (
     <main className="flex justify-center items-center w-full h-screen">
       <div className="text-center">
-        <Button type="primary">Create Room</Button>
+        <Input placeholder="Enter Your Name" className="mb-4" />
+        <Button type="primary" onClick={handleCreateRoom}>
+          Create Room
+        </Button>
         <Divider>OR</Divider>
-        <Input placeholder="Enter Room ID" className="mb-4" />
-        <Button type="primary">Join Room</Button>
+        <Form autoComplete="off" className="mb-4">
+          <Form.Item name="roomID">
+            <Input placeholder="Enter Room ID" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Join Room
+          </Button>
+        </Form>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
